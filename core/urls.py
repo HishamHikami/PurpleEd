196a0549e61622ed18d87e815a0c42967a13fa2f
HishamHikami <hishamhikami@duck.com>
2022-08-25 02:53:02 +0000

    added heroku files

--- core/urls.py
+++ core/urls.py
@@ -7,13 +7,13 @@ from django.contrib import admin
 from django.urls import path, include  # add this
 
 urlpatterns = [
-    path('admin/', admin.site.urls),          # Django admin route
-    path("", include("apps.authentication.urls")), # Auth routes - login / register
+    path("", admin.site.urls),          # Django admin route
+   # path("", include("apps.authentication.urls")), # Auth routes - login / register
 
     # ADD NEW Routes HERE
 
     # Leave `Home.Urls` as last the last line
-    path("", include("apps.home.urls"))
+    # path("", include("apps.home.urls"))
 ]
 
 admin.site.site_title = "Site Title"
